8a6e307c4847da0d0edbdc6afbe2e2424349fb00
sahil sharma <113684157+ayushkhanduri78@users.noreply.github.com>
2023-11-01 11:25:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,8 +47,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 support_chat_id = environ.get('SUPPORT_CHAT_ID')
 reqst_channel = environ.get('REQST_CHANNEL_ID')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-REQST_CHANNEL_ID = '-1002079354717'
-SUPPORT_CHAT_ID = '-1002079354717'
+REQST_CHANNEL_ID = -1002079354717
+SUPPORT_CHAT_ID = -1002079354717
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
