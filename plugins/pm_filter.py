8b11f5766a878259e61b40ddb01dbda193f9f9d0
sahil sharma <113684157+ayushkhanduri78@users.noreply.github.com>
2023-11-03 03:35:49 +0000

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -97,7 +97,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                   text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                   text=f"❄{get_size(file.file_size)}✨{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1288,7 +1288,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"❄{get_size(file.file_size)}✨{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
